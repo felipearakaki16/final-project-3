109e28d2e2bee0002a0d194168b562508c206b97
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-09 14:58:31 -0300

    Associations and validations changed.

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,2 +1,7 @@
 class Level < ApplicationRecord
+  has_many :user_records, dependent: :destroy
+  has_many :levels, through: :user_records
+
+  validates :name, presence: true
+  validates :dificulty, presence: true
 end
