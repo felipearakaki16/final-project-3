c8be47de64d42b937cbc4fb66f545c1badfa6000
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-11 14:34:51 -0300

    Transfered logic from user_records/index to designated controller.

--- app/controllers/user_records_controller.rb
+++ app/controllers/user_records_controller.rb
@@ -4,6 +4,19 @@ class UserRecordsController < ApplicationController
   def index
     @user_records = UserRecord.where(user: current_user)
     @user = User.find(current_user.id)
+
+    @level_1_created = !@user_records.where(level_id: 1).first.nil? ? @user_records.where(level_id: 1).first.created_at.strftime("%d/%b/%Y - %Hh%Mm") : ""
+    @level_1_updated = !@user_records.where(level_id: 1).first.nil? ? @user_records.where(level_id: 1).first.updated_at.strftime("%d/%b/%Y - %Hh%Mm") : ""
+    @level_1_completed = !@user_records.where(level_id: 1).first.completed_at.nil? ? @user_records.where(level_id: 1).first.completed_at.strftime("%d/%b/%Y - %Hh%Mm") : ""
+
+    @level_2_created = !@user_records.where(level_id: 2).first.nil? ? @user_records.where(level_id: 2).first.created_at.strftime("%d/%b/%Y - %Hh%Mm") : ""
+    @level_2_updated = !@user_records.where(level_id: 2).first.nil? ? @user_records.where(level_id: 2).first.updated_at.strftime("%d/%b/%Y - %Hh%Mm") : ""
+    @level_2_completed = !@user_records.where(level_id: 2).first.completed_at.nil? ? @user_records.where(level_id: 2).first.completed_at.strftime("%d/%b/%Y - %Hh%Mm") : ""
+
+
+    @level_3_created = !@user_records.where(level_id: 3).first.nil? ? @user_records.where(level_id: 3).first.created_at.strftime("%d/%b/%Y - %Hh%Mm") : ""
+    @level_3_updated = !@user_records.where(level_id: 3).first.nil? ? @user_records.where(level_id: 3).first.updated_at.strftime("%d/%b/%Y - %Hh%Mm") : ""
+    @level_3_completed = !@user_records.where(level_id: 3).first.completed_at.nil? ? @user_records.where(level_id: 3).first.completed_at.strftime("%d/%b/%Y - %Hh%Mm") : ""
   end
 
   def update
