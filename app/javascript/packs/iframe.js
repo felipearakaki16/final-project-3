dcf0fe78de54134ec6036434d11d07e3b370779a
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-15 15:43:52 -0300

    iframe error

--- app/javascript/packs/iframe.js
+++ app/javascript/packs/iframe.js
@@ -2,6 +2,6 @@ const loadIframe = () => {
   const iframeQuestion = document.getElementById('iframe-question')
   if ( iframeQuestion ) {
     const iframeContent = iframeQuestion.contentDocument
-    iframeContent.getElementById('page-container').innerHTML = 
+    // iframeContent.getElementById('page-container').innerHTML = "";
   }
 }
