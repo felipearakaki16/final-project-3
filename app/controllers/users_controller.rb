472967dfdbec1c82dea40aa146ec2139eadcd694
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-16 16:58:12 -0300

    Fixed users_controller show to display user infos.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_path unless current_user == @user 
+    redirect_to root_path unless current_user == @user
+    @levels = []
+    Level.all.each do |level|
+      hash = {}
+      hash[:level] = level
+      hash[:record] = UserRecord.find_by(user: current_user, level: level)
+      @levels << hash
+    end
    end
 end
