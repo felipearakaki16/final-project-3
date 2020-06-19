0f2b40f20ef9cd55f397cebc2116236f3c26caad
Isabela Porto <isamporto@hotmail.com>
2020-06-19 15:17:12 -0300

    Fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,26 @@
 Level.destroy_all
-  # puts 'Creating level'
-  # level1 = Level.new(id: 1, name: "Level 1", description: "Display", correction: ["display: *flex;"], difficulty: "easy")
-  # level1.save!
-  # puts 'Finished'
+
 puts 'Creating level'
-level2 = Level.new(id: 2, name: "Level 2", description: "Flex inside flex", correction: ["display: *flex;"], difficulty: "easy")
+level1 = Level.new(id: 1, name: "Level 1", description: "Display", correction: ["display: *flex;"], difficulty: "easy")
 level1.save!
 puts 'Finished'
 
+puts 'Creating level'
+level2 = Level.new(id: 2, name: "Level 2", description: "Flex inside flex", correction: ["display: *flex;"], difficulty: "easy")
+level2.save!
+puts 'Finished'
+
 puts 'Creating example'
-landing_page = Example.new(id: 1, level: level1, target: "querySelector('.navbar-ex')")
+landing_page = Example.new(id: 1, level_id: 1, target: "querySelector('.navbar-ex')")
 landing_page.content = '<div class="navbar-ex">
 <img class="logo" src="/images/pacman-logo.png">
 <ul>
-  <li><a href="#" onclick="event.preventDefault">HOME</a></li>
-  <li><a href="#" onclick="event.preventDefault">ABOUT</a></li>
-  <li><a href="#" onclick="event.preventDefault">FAQ</a></li>
-  <li><a href="#" onclick="event.preventDefault">CONTACT</a></li>
-  <li><a href="#" onclick="event.preventDefault">GALLERY</a></li>
-  <li><a href="#" onclick="event.preventDefault">TEAM</a></li>
+  <li><a href="#" onclick="return false">HOME</a></li>
+  <li><a href="#" onclick="return false">ABOUT</a></li>
+  <li><a href="#" onclick="return false">FAQ</a></li>
+  <li><a href="#" onclick="return false">CONTACT</a></li>
+  <li><a href="#" onclick="return false">GALLERY</a></li>
+  <li><a href="#" onclick="return false">TEAM</a></li>
 </ul>
 </div>
 <div id="content-wrap">
@@ -58,36 +60,36 @@ landing_page.content = '<div class="navbar-ex">
 <div>
 <h3>Social Media</h3>
 <div>
-  <a href="#" onclick="event.preventDefault"><img class="icon" src="/images/facebook.png"></a>
-  <a href="#" onclick="event.preventDefault"><img class="icon" src="/images/instagram.png"></a>
-  <a href="#" onclick="event.preventDefault"><img class="icon" src="/images/linkedin.png"></a>
-  <a href="#" onclick="event.preventDefault"><img class="icon" src="/images/twitter.png"></a>
+  <a href="#" onclick="return false"><img class="icon" src="/images/facebook.png"></a>
+  <a href="#" onclick="return false"><img class="icon" src="/images/instagram.png"></a>
+  <a href="#" onclick="return false"><img class="icon" src="/images/linkedin.png"></a>
+  <a href="#" onclick="return false"><img class="icon" src="/images/twitter.png"></a>
 </div>
 </div>
 <img class="pacman" src="/images/Pacman.png">
 <div>
 <h3>Important links</h3>
 <ul>
-  <li><a href="#" onclick="event.preventDefault">a link</a></li>
-  <li><a href="#" onclick="event.preventDefault">another link</a></li>
-  <li><a href="#" onclick="event.preventDefault">one more link</a></li>
-  <li><a href="#" onclick="event.preventDefault">last link</a></li>
+  <li><a href="#" onclick="return false">a link</a></li>
+  <li><a href="#" onclick="return false">another link</a></li>
+  <li><a href="#" onclick="return false">one more link</a></li>
+  <li><a href="#" onclick="return false">last link</a></li>
 </ul>
 </div>
 </footer>'
 landing_page.save
 puts 'Finished'
 puts 'Creating example'
-landing_page = Example.new(id: 2, level: level2, target: "querySelector('.navbar-ex ul')")
-landing_page.content = '<div class="navbar-ex d-flex">
+landing = Example.new(id: 2, level_id: 2, target: "querySelector('.navbar-ex ul')")
+landing.content = '<div class="navbar-ex d-flex">
 <img class="logo" src="/images/pacman-logo.png">
 <ul>
-  <li><a href="#" onclick="event.preventDefault">HOME</a></li>
-  <li><a href="#" onclick="event.preventDefault">ABOUT</a></li>
-  <li><a href="#" onclick="event.preventDefault">FAQ</a></li>
-  <li><a href="#" onclick="event.preventDefault">CONTACT</a></li>
-  <li><a href="#" onclick="event.preventDefault">GALLERY</a></li>
-  <li><a href="#" onclick="event.preventDefault">TEAM</a></li>
+  <li><a href="#" onclick="return false">HOME</a></li>
+  <li><a href="#" onclick="return false">ABOUT</a></li>
+  <li><a href="#" onclick="return false">FAQ</a></li>
+  <li><a href="#" onclick="return false">CONTACT</a></li>
+  <li><a href="#" onclick="return false">GALLERY</a></li>
+  <li><a href="#" onclick="return false">TEAM</a></li>
 </ul>
 </div>
 <div id="content-wrap">
@@ -127,22 +129,22 @@ landing_page.content = '<div class="navbar-ex d-flex">
 <div>
 <h3>Social Media</h3>
 <div>
-  <a href="#" onclick="event.preventDefault"><img class="icon" src="/images/facebook.png"></a>
-  <a href="#" onclick="event.preventDefault"><img class="icon" src="/images/instagram.png"></a>
-  <a href="#" onclick="event.preventDefault"><img class="icon" src="/images/linkedin.png"></a>
-  <a href="#" onclick="event.preventDefault"><img class="icon" src="/images/twitter.png"></a>
+  <a href="#" onclick="return false"><img class="icon" src="/images/facebook.png"></a>
+  <a href="#" onclick="return false"><img class="icon" src="/images/instagram.png"></a>
+  <a href="#" onclick="return false"><img class="icon" src="/images/linkedin.png"></a>
+  <a href="#" onclick="return false"><img class="icon" src="/images/twitter.png"></a>
 </div>
 </div>
 <img class="pacman" src="/images/Pacman.png">
 <div>
 <h3>Important links</h3>
 <ul>
-  <li><a href="#" onclick="event.preventDefault">a link</a></li>
-  <li><a href="#" onclick="event.preventDefault">another link</a></li>
-  <li><a href="#" onclick="event.preventDefault">one more link</a></li>
-  <li><a href="#" onclick="event.preventDefault">last link</a></li>
+  <li><a href="#" onclick="return false">a link</a></li>
+  <li><a href="#" onclick="return false">another link</a></li>
+  <li><a href="#" onclick="return false">one more link</a></li>
+  <li><a href="#" onclick="return false">last link</a></li>
 </ul>
 </div>
 </footer>'
-landing_page.save
+landing.save
 puts 'Finished'
